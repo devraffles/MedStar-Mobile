95adb4856fd4402a0f302ef6469dfccd1139c7fe
devraffles <devraffles@gmail.com>
2024-10-15 16:10:27 -0300

    Medstar-mobile-v1

---
+++ src/components/button/button.style.js
@@ -0,0 +1,18 @@
+import { COLORS, FONT_SIZE } from "../../constants/theme.js";
+
+export const styles = {
+
+    btn: {
+        width: "100%",
+        backgroundColor: COLORS.green,
+        borderRadius: 8,
+        padding: 12
+    },
+
+    text: {
+        color: COLORS.gray1,
+        fontSize: FONT_SIZE.md,
+        textAlign: "center"
+    }
+
+}
---
+++ src/screens/login/login.style.js
@@ -0,0 +1,38 @@
+import { COLORS, FONT_SIZE } from "../../constants/theme.js";
+
+export const styles = {
+    container: {
+        backgroundColor: COLORS.white,
+        flex: 1,
+        padding: 50,
+        justifyContent: "space-between"
+    },
+    containerLogo: {
+        alignItems: "center"
+    },
+    logo: {
+        marginTop: 10,
+        width: 151,
+        height: 40
+    },
+    containerInput: {
+        marginBottom: 15
+    },
+    input: {
+        backgroundColor: COLORS.gray5,
+        padding: 10,
+        borderRadius: 8
+    },
+    footer: {
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "colum",
+        gap: 14
+    },
+    footerLink: {
+        color: COLORS.green,
+        backgroundColor: COLORS.gray1,
+        padding: 14,
+        borderRadius: 8
+    }
+}
